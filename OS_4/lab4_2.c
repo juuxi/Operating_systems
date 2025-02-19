e2ff762ed8127d7464a7a7e964e4d84d7207272c
Dmitry Shopyrev <dimashopyrev@mail.ru>
2025-02-20 01:01:30 +0300

    linked processes OS_4

--- OS_4/lab4_2.c
+++ OS_4/lab4_2.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
@@ -9,7 +10,8 @@ int main(int argc, char* argv[]) {
     pid_t pid = fork();
     int status;
     if (pid == 0) {
-        if (execlp("lab4_1", "lab4_1", argv[1], NULL) == -1) {
+        putenv("PATH=./");
+        if (execlp("lab4_1", argv[0], NULL) == -1) {
             perror("");
         }
     } else if (pid > 0) {
