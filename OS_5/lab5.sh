e9d6b0869676cfe8ad17f291bc7295c23f1a7f5a
Dmitry Shopyrev <dimashopyrev@mail.ru>
2025-02-23 13:21:02 +0300

    OS_5 init

---
+++ OS_5/lab5.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+gcc	-c	lab5_1.c
+gcc	-o	lab5_1	lab5_1.o		-lpthread
+gcc	-c	lab5_2.c
+gcc	-o	lab5_2	lab5_2.o		-lpthread
---
+++ OS_5/lab5_2.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <semaphore.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/select.h>
+
+int main () {
+    /* sem_t* sem = sem_open("/my_sem", 0); */
+    int file_dis;
+    file_dis = open("./lab5.txt", O_CREAT);
+    fd_set rfds;
+    FD_SET(0, &rfds);
+    struct timeval timeout;
+    timeout.tv_sec = 1;
+    while (1) {
+        for (int i = 0; i < 10; i++) { //Вход в КУ
+            write(file_dis, "2", sizeof(char));
+            printf("2");
+            fflush(stdout);
+            sleep(1);
+        } //Выход из КУ
+        sleep(1);
+        if (select(1, &rfds, NULL, NULL, &timeout) == 1) {
+            break;
+        }
+    }
+    close(file_dis);
+}
