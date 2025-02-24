124d17d9e1c8891375867e273c76b501a7f245a0
Dmitry Shopyrev <dimashopyrev@mail.ru>
2025-02-24 14:03:57 +0300

    further work on OS_5

--- OS_5/lab5_1.c
+++ OS_5/lab5_1.c
@@ -7,25 +7,32 @@
 #include <sys/select.h>
 
 int main () {
-    /* sem_t* sem = sem_open("/my_sem", 0); */
+    sem_t* sem = sem_open("/my_sem", O_CREAT, O_RDWR, 1);
     int file_dis;
-    file_dis = open("./lab5.txt", O_CREAT);
+    file_dis = open("./lab5.txt", O_CREAT|O_WRONLY);
     fd_set rfds;
     FD_SET(0, &rfds);
     struct timeval timeout;
     timeout.tv_sec = 1;
     while (1) {
+        sem_wait(sem);
         for (int i = 0; i < 10; i++) { //Вход в КУ
             char buf = '1';
-            write(file_dis, &buf, sizeof(char));
+            if (write(file_dis, &buf, sizeof(char)) == -1)
+                perror("");
             printf("1");
             fflush(stdout);
             sleep(1);
         } //Выход из КУ
+        sem_post(sem);
         sleep(1);
-        if (select(1, &rfds, NULL, NULL, &timeout) == 1) {
+        if (select(1, &rfds, NULL, NULL, &timeout) != 0) {
             break;
+        } else {
+            FD_SET(0, &rfds);
         }
     }
     close(file_dis);
+    sem_close(sem);
+    sem_unlink("/my_sem");
 }
