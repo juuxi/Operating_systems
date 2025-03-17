cc002fd64edd8054bebb9ed5a16552dccecdf815
Dmitry Shopyrev <dimashopyrev@mail.ru>
2025-03-17 12:44:10 +0300

    OS_8 almost done

--- OS_8/lab8_1.c
+++ OS_8/lab8_1.c
@@ -5,6 +5,7 @@
 #include <string.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <stddef.h>
 #include <fcntl.h>
 #include <sys/types.h>
 #include <sys/socket.h>
@@ -16,8 +17,17 @@
 #include <ctype.h>
 
 int flag_rcv = 0;
+int flag_process = 0;
 int client_sock;
+int client_sock_2;
 struct sockaddr_un addr;
+
+struct entry {
+    char* msg;
+    STAILQ_ENTRY(entry) entries;
+};
+struct stailhead head;
+STAILQ_HEAD(stailhead, entry);
 
 void * func1() {
     printf("поток приема начал работу\n");
@@ -27,42 +37,66 @@ void * func1() {
         addr.sun_family = AF_UNIX;
         strncpy(addr.sun_path,"socket.soc",sizeof(addr.sun_path) - 1);
         int rv = recvfrom(client_sock, &rcv_msg, sizeof(rcv_msg), 0, (struct sockaddr*)&addr, &slen);
-        char* iter = rcv_msg;
+        
         long val = 0;
         int i = 0;
-        while (!isdigit(*iter)) {
-            iter++;
-        }
-        while (isdigit(*iter)) {
-            i = i*10+*iter-'0';
-            iter++;
-        }
-        while (!isdigit(*iter)) {
-            iter++;
-        }
-        while (isdigit(*iter)) {
-            val = val*10+*iter-'0';
-            iter++;
-        }
-        if (rv == -1)
+        if (rv == -1) {
             perror("rcv");
+            sleep(1);
+        }
         else {
-            printf("%s\n", rcv_msg);
-            printf("%d\n", i);
-            printf("%ld\n", val);
+            struct entry *ent_1;
+            ent_1 = malloc(sizeof(struct entry));
+            ent_1->msg = rcv_msg;
+            STAILQ_INSERT_TAIL(&head, ent_1, entries);
         }
         sleep(1);
     }
     printf("поток приема закончил работу\n");
 }
 
+void * func2() {
+    printf("поток обработки начал работу\n");
+    while(flag_process == 0) {
+        int i = 0;
+        if (!STAILQ_EMPTY(&head)) {
+            struct entry *first_ent;
+            first_ent = STAILQ_FIRST(&head);
+            STAILQ_REMOVE_HEAD(&head, entries);
+            char* iter = first_ent->msg;
+            while (!isdigit(*iter)) {
+                iter++;
+            }
+            while (isdigit(*iter)) {
+                i = i*10+*iter-'0';
+                iter++;
+            }
+            printf("Сообщение %d принято\n", i);
+            free(first_ent);
+            long func_res = pathconf("./", _PC_NAME_MAX);
+            char send_msg[50];
+            sprintf(send_msg, "Сообщение %ld отправлено", func_res);
+            struct sockaddr_un addr2;
+            addr2.sun_family = AF_UNIX;
+            strncpy(addr2.sun_path,"socket2.soc",sizeof(addr2.sun_path) - 1);
+            int rv = sendto(client_sock_2, &send_msg, sizeof(send_msg), 0, (struct sockaddr*)&addr2, sizeof(addr2));
+            if (rv == -1) {
+                perror("send");
+            }
+        }
+    }
+    printf("поток обработки закончил работу\n");
+}
+
 int main() {
     printf("программа сервера работу\n");
-    pthread_t id1;
+    pthread_t id1, id2;
 
     client_sock = socket(AF_UNIX, SOCK_DGRAM, 0);
     fcntl(client_sock, F_SETFL, O_NONBLOCK);
-    struct sockaddr_un addr;
+    client_sock_2 = socket(AF_UNIX, SOCK_DGRAM, 0);
+    fcntl(client_sock_2, F_SETFL, O_NONBLOCK);
+
     addr.sun_family = AF_UNIX;
     strncpy(addr.sun_path,"socket.soc",sizeof(addr.sun_path) - 1);
     int rv = bind(client_sock, (struct sockaddr*)&addr, sizeof(addr));
@@ -73,15 +107,23 @@ int main() {
     setsockopt(client_sock, SOL_SOCKET, SO_REUSEADDR,
         &optval, sizeof(optval));
 
+    STAILQ_INIT(&head);
+
+
     pthread_create(&id1, NULL, func1, NULL);
+    pthread_create(&id2, NULL, func2, NULL);
     printf("программа ждет нажатия клавиши\n");
     getchar();
     printf("клавиша нажата\n");
     flag_rcv = 1;
+    flag_process = 1;
     pthread_join(id1, NULL);
+    pthread_join(id2, NULL);
 
     close(client_sock);
     unlink("socket.soc");
+    close(client_sock_2);
+    unlink("socket2.soc");
 
     printf("программа сервера работу\n");
 }
--- OS_8/lab8_2.c
+++ OS_8/lab8_2.c
@@ -14,17 +14,19 @@
 #include <sys/queue.h>
 #include <sys/stat.h>
 
-int flag1 = 0;
+int flag_send = 0;
+int flag_receive = 0;
 int server_sock;
+int server_sock_2;
 struct sockaddr_un addr;
+struct sockaddr_un addr2;
 
 void * func1() {
-    printf("поток начал работу\n");
+    printf("поток отправки запросов начал работу\n");
     char send_msg[50];
     int i = 1;
-    while(flag1 == 0) {
-        long func_res = pathconf("./", _PC_NAME_MAX);
-        sprintf(send_msg, "Сообщение %d отправлено: %ld", i, func_res);
+    while(flag_send == 0) {
+        sprintf(send_msg, "Сообщение %d отправлено", i);
         addr.sun_family = AF_UNIX;
         strncpy(addr.sun_path,"socket.soc",sizeof(addr.sun_path) - 1);
         int rv = sendto(server_sock, &send_msg, sizeof(send_msg), 0, (struct sockaddr*)&addr, sizeof(addr));
@@ -38,34 +40,61 @@ void * func1() {
         }
         i++;
     }
-    printf("поток закончил работу\n");
+    printf("поток отправки запросов закончил работу\n");
+}
+
+void * func2() {
+    printf("поток обработки ответов начал работу\n");
+    while (flag_receive == 0) {
+        char rcv_msg[50];
+        socklen_t slen = sizeof(addr2);
+        addr2.sun_family = AF_UNIX;
+        strncpy(addr2.sun_path,"socket2.soc",sizeof(addr2.sun_path) - 1);
+        int rv = recvfrom(server_sock_2, &rcv_msg, sizeof(rcv_msg), 0, (struct sockaddr*)&addr2, &slen);
+        if (rv == -1) {
+            perror("receive");
+            sleep(1);
+        }
+        else {
+            printf("%s\n", rcv_msg);
+        }
+    }
+    printf("поток обработки ответов закончил работу\n");
 }
 
 int main() {
     printf("программа сервера работу\n");
-    pthread_t id1;
+    pthread_t id1, id2;
 
     server_sock = socket(AF_UNIX, SOCK_DGRAM, 0);
     fcntl(server_sock, F_SETFL, O_NONBLOCK);
-    /* addr.sun_family = AF_UNIX;
-    strncpy(addr.sun_path,"socket.soc",sizeof(addr.sun_path) - 1);
-    int rv = bind(server_sock, (struct sockaddr*)&addr, sizeof(addr));
+    server_sock_2 = socket(AF_UNIX, SOCK_DGRAM, 0);
+    fcntl(server_sock_2, F_SETFL, O_NONBLOCK);
+
+    addr2.sun_family = AF_UNIX;
+    strncpy(addr2.sun_path,"socket2.soc",sizeof(addr2.sun_path) - 1);
+    int rv = bind(server_sock_2, (struct sockaddr*)&addr2, sizeof(addr2));
     if (rv == -1) {
         perror("bind");
     }
     int optval = 1;
     setsockopt(server_sock, SOL_SOCKET, SO_REUSEADDR,
-        &optval, sizeof(optval)); */
+        &optval, sizeof(optval));
 
     pthread_create(&id1, NULL, func1, NULL);
+    pthread_create(&id2, NULL, func2, NULL);
     printf("программа ждет нажатия клавиши\n");
     getchar();
     printf("клавиша нажата\n");
-    flag1 = 1;
+    flag_send = 1;
+    flag_receive = 1;
     pthread_join(id1, NULL);
+    pthread_join(id2, NULL);
 
     close(server_sock);
     unlink("socket.soc");
+    close(server_sock_2);
+    unlink("socket2.soc");
 
     printf("программа сервера работу\n");
 }
